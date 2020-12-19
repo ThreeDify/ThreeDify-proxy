480bf2e7201bf7d6144a5062e59b8607d0ee27b6
silwalanish <silwalanish@gmail.com>
2020-12-19 17:44:03 +0545

    (chore): Add app credentials.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import cors from 'cors';
 import morgan from 'morgan';
+import { RequestOptions } from 'http';
 import proxy from 'express-http-proxy';
 import Debug, { Debugger } from 'debug';
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 
 import config from './config';
 
@@ -17,11 +18,18 @@ app.use(cors(config.corsConfig));
 app.use(
   proxy(config.apiBaseUrl, {
     parseReqBody: false,
-    filter: (req, res) => {
+    filter: (req: Request, res: Response) => {
       return new Promise((resolve) => {
         debug('Check if cors passes... %s: %s', req.method, req.url);
         resolve(res.hasHeader('access-control-allow-origin'));
       });
+    },
+    proxyReqOptDecorator: (proxyReqOpts: RequestOptions, srcReq: Request) => {
+      if (proxyReqOpts.headers) {
+        proxyReqOpts.headers['x-threedify-app-id'] = config.appId;
+        proxyReqOpts.headers['x-threedify-app-secret'] = config.appSecret;
+      }
+      return proxyReqOpts;
     },
     limit: '6mb',
   })
--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,9 @@ import { CorsOptions } from 'cors';
 
 interface Config {
   port: number;
+  appId: string;
   baseUrl: string;
+  appSecret: string;
   apiBaseUrl: string;
   corsConfig: CorsOptions;
   requestLogFormat: string;
@@ -10,7 +12,9 @@ interface Config {
 
 const config: Config = {
   requestLogFormat: 'tiny',
+  appId: process.env.APP_ID || '',
   baseUrl: process.env.BASE_URL || '',
+  appSecret: process.env.APP_SECRET || '',
   apiBaseUrl: process.env.API_BASE_URL || '',
   port: +(process.env?.PORT || 3000),
   corsConfig: {
